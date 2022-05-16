3cdcc2bd14baf80acae934bf2db198a85fefe175
Benjamin McCown <mccownbm@amazon.com>
2022-05-16 11:24:23 -0500

    feat: install roam v2

--- doom-emacs/config.el
+++ doom-emacs/config.el
@@ -19,7 +19,6 @@
   (setq org-directory "~/Documents/doom-org")
   (setq org-agenda-files (directory-files-recursively "~/Documents/doom-org" "\\.org$"))
   (setq org-default-inbox-file "~/Documents/doom-org/todo.org")
-  (setq org-roam-directory "~/Documents/doom-org/notes")
 )
 
 (use-package! evil-easymotion
@@ -29,8 +28,63 @@
   (evilem-default-keybindings "g SPC")
 )
 
+(after! org-roam
+  (setq org-roam-directory "~/Documents/doom-org/notes")
+  )
+
+(after! org-roam
+  (setq org-roam-capture-templates
+      '(("m" "main" plain
+         "%?"
+         :if-new (file+head "main/${slug}.org"
+                            "#+title: ${title}\n")
+         :immediate-finish t
+         :unnarrowed t)
+        ("r" "reference" plain "%?"
+         :if-new
+         (file+head "reference/${title}.org" "#+title: ${title}\n")
+         :immediate-finish t
+         :unnarrowed t)
+        ("a" "article" plain "%?"
+         :if-new
+         (file+head "articles/${title}.org" "#+title: ${title}\n#+filetags: :article:\n")
+         :immediate-finish t
+         :unnarrowed t)))
+  )
+
+(after! org-roam
+  (cl-defmethod org-roam-node-type ((node org-roam-node))
+    "Return the TYPE of NODE."
+    (condition-case nil
+        (file-name-nondirectory
+         (directory-file-name
+          (file-name-directory
+           (file-relative-name (org-roam-node-file node) org-roam-directory))))
+      (error "")))
+  (setq org-roam-node-display-template
+      (concat "${type:15} ${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
+  )
+
 (after! org
+
   (setq org-log-into-drawer "LOGBOOK")
+
+  (setq org-capture-templates
+        '(
+        ("a" "AppFlow project item" entry (file+headline "~/Documents/doom-org/appflow-replacement.org" "AppFlow Epic")
+           "* TODO %(prio) %?\nSCHEDULED: %^t\n:PROPERTIES:\n:CREATED: %U\n:END:\n:LOGBOOK:\n:END:\n" :empty-lines-before 1 :empty-lines-after 1)
+        ("b" "Backlog item" entry (file org-default-inbox-file)
+           "* TODO [#5] %?\n:PROPERTIES:\n:CREATED: %U\n:END:\n:LOGBOOK:\n:END:\n" :empty-lines-before 1 :empty-lines-after 1)
+        ("s" "Scheduled todo item" entry (file org-default-inbox-file)
+           "* TODO %(prio) %?\nSCHEDULED: %^t\n:PROPERTIES:\n:CREATED: %U\n:END:\n:LOGBOOK:\n:END:\n" :empty-lines-before 1 :empty-lines-after 1)
+        ("n" "Notes slipbox" entry  (file "braindump/inbox.org")
+         "* %?\n")
+        )
+        )
+
+  (setq org-log-done 'time)
+  (setq org-hide-emphasis-markers t)
+  (add-hook 'org-mode-hook 'visual-line-mode)
 
   (setq org-priority-highest 1)
   (setq org-priority-lowest 5)
@@ -40,33 +94,18 @@
   (format "[#%d]" org-priority-default))
 
   (setq org-todo-keywords
-        '((sequence "TODO(t)" "FOLLOWUP_ITEM(f@/!)" "IN_PROGRESS(g!/!)" "OPEN_CR(c@)" "BLOCKED(b@)" "|" "DONE(d!)" "OBE(e@)" "DELEGATED(p@)" "DROPPED(x@)")))
+        '((sequence "TODO(t)" "FOLLOWUP_ITEM(f@/!)" "IN_PROGRESS(g!/!)" "OPEN_CR(c@)" "UNDER_REVIEW(r@)" "BLOCKED(b@)" "|" "DONE(d!)" "OBE(e@)" "DELEGATED(p@)" "DROPPED(x@)")))
   (setq org-todo-keywords-for-agenda
-        '((sequence "TODO(t)" "FOLLOWUP_ITEM(f@/!)" "IN_PROGRESS(g!/!)" "OPEN_CR(c@)" "BLOCKED(b@)" "|" "DONE(d!)" "OBE(e@)" "DELEGATED(p@)" "DROPPED(x@)")))
-
-  (setq org-capture-templates
-        '(
-        ("a" "AppFlow Project Item" entry (file+headline "~/Documents/doom-org/appflow-replacement.org" "AppFlow Epic")
-           "* TODO %(prio) %?\nSCHEDULED: %^t\n:PROPERTIES:\n:CREATED: %U\n:END:\n:LOGBOOK:\n:END:\n" :empty-lines-before 1 :empty-lines-after 1)
-        ("b" "Backlog Item" entry (file org-default-inbox-file)
-           "* TODO [#5] %?\n:PROPERTIES:\n:CREATED: %U\n:END:\n:LOGBOOK:\n:END:\n" :empty-lines-before 1 :empty-lines-after 1)
-        ("s" "Scheduled ToDo Item" entry (file org-default-inbox-file)
-           "* TODO %(prio) %?\nSCHEDULED: %^t\n:PROPERTIES:\n:CREATED: %U\n:END:\n:LOGBOOK:\n:END:\n" :empty-lines-before 1 :empty-lines-after 1)
-        )
-        )
-
-  (setq org-log-done 'time)
-  (setq org-hide-emphasis-markers t)
-  (add-hook 'org-mode-hook 'visual-line-mode)
+        '((sequence "TODO(t)" "FOLLOWUP_ITEM(f@/!)" "IN_PROGRESS(g!/!)" "OPEN_CR(c@)" "UNDER_REVIEW(r@)" "BLOCKED(b@)" "|" "DONE(d!)" "OBE(e@)" "DELEGATED(p@)" "DROPPED(x@)")))
 
 )
 
-(map! :after evil-org
-      :map evil-org-mode-map
-      :localleader
-      (:prefix-map ("z" . "custom")
-       :desc "Toggle hide drawer" "a" #'org-hide-drawer-toggle)
-      )
+;; (map! :after evil-org-mode
+;;       :map evil-org-mode-map
+;;       :localleader
+;;       (:prefix-map ("z" . "custom")
+;;        :desc "Toggle hide drawer" "a" #'org-hide-drawer-toggle)
+;;       )
 
 (after! evil-org
 (push '("cu" "Unscheduled TODO"
